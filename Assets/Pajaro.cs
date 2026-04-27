cf381d601d32ddd2fc1ef2177f28035e5a8c0474
adrian tinaquero <adrian.tinaquero@udc.es>
2026-04-27 11:07:07 +0200

    feat: pajaro inicia ContractNet si ve al ladrón

--- Assets/Pajaro.cs
+++ Assets/Pajaro.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 public class Pajaro : MonoBehaviour, InterfazAgenteComunicativo
 {
 
+    private Dictionary<GameObject, float> propuestas = new Dictionary<GameObject, float>();
+    private int ID_actual = 0;
+    private bool esperandoRespuestas = false;
+
+
     void Start()
     {
         AgenteComunicativo.RegistrarAgente(this);
@@ -18,13 +23,89 @@ public class Pajaro : MonoBehaviour, InterfazAgenteComunicativo
     public void VeAlLadron(Vector3 posicion)
     {
         Debug.Log("Pájaro ha visto al ladrón");
+
+        propuestas.Clear();
+        ID_actual++;
+        esperandoRespuestas = true;
+
         AgenteComunicativo.EnviarBroadcast(
-            new Mensaje{Emisor = gameObject, intencion = Intencion.Inform, Contenido = "Ladron visto"});
+            new Mensaje
+            {
+                Emisor = gameObject,
+                intencion = Intencion.Cfp,
+                Contenido = posicion.x + "," + posicion.y + "," + posicion.z,
+                IDConversacion = ID_actual
+            });
+
+        Invoke(nameof(SeleccionarGanador), 1.0f);
     }
 
+    void SeleccionarGanador()
+    {
+        if (!esperandoRespuestas) return;
+
+        esperandoRespuestas = false;
+
+        if (propuestas.Count == 0)
+        {
+            Debug.Log("Ningún guardián respondió");
+            return;
+        }
+
+        GameObject mejorGuardian = null;
+        float mejorValor = Mathf.Infinity;
+
+        foreach (var propuesta in propuestas)
+        {
+            if (propuesta.Value < mejorValor)
+            {
+                mejorValor = propuesta.Value;
+                mejorGuardian = propuesta.Key;
+            }
+        }
+
+        foreach (var propuesta in propuestas)
+        {
+            if (propuesta.Key == mejorGuardian)
+            {
+                AgenteComunicativo.EnviarDirecto(
+                    propuesta.Key,
+                    new Mensaje
+                    {
+                        Emisor = gameObject,
+                        Receptor = propuesta.Key,
+                        intencion = Intencion.AcceptProposal,
+                        IDConversacion = ID_actual
+                    });
+            }
+            else
+            {
+                AgenteComunicativo.EnviarDirecto(
+                    propuesta.Key,
+                    new Mensaje
+                    {
+                        Emisor = gameObject,
+                        Receptor = propuesta.Key,
+                        intencion = Intencion.RejectProposal,
+                        IDConversacion = ID_actual
+                    });
+            }
+        }
+
+        Debug.Log("Ganador: " + mejorGuardian.name);
+    }
 
     public void RecibirMensaje(Mensaje mensaje)
     {
+        if (mensaje.intencion == Intencion.Propose &&
+            mensaje.IDConversacion == ID_actual)
+        {
+            float valor = float.Parse(mensaje.Contenido);
+
+            propuestas[mensaje.Emisor] = valor;
+
+            Debug.Log("Propuesta de " + mensaje.Emisor.name + ": " + valor);
+        }
     }
 
     public GameObject GetGameObject()
