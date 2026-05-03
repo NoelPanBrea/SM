58c7c7d346dc0717432e53994d2c5c0af2a2596c
adrian tinaquero <adrian.tinaquero@udc.es>
2026-05-03 19:22:53 +0200

    feat: capa comunicacion

--- Assets/Cerebro.cs
+++ Assets/Cerebro.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 
 
-public class Cerebro : MonoBehaviour, InterfazAgenteComunicativo
+public class Cerebro : MonoBehaviour
 {
     public Estado estado;
     public Patrullar patrullar;
@@ -16,23 +16,19 @@ public class Cerebro : MonoBehaviour, InterfazAgenteComunicativo
     public Transform PuntoHuidaLadron;
     public Transform ladron;
     public Cerebro_ladrón LadrontieneTesoro;
+    private ComunicacionGuardian comunicacion;
     private float tiempoUltimaVision = -Mathf.Infinity;
     private float tiempoUltimaInvestigación = -Mathf.Infinity;
     private float tiempoUltimaComprobacion;
     private Vector3 ultimaPosicionConocida;
     private Vector3 puntoInvestigacion;
     private NavMeshAgent agent;
-    private Dictionary<GameObject, float> propuestas = new Dictionary<GameObject, float>();
-    private int ID_actual = 0;
-    private bool esperandoRespuestas = false;
-    private Dictionary<int, List<Mensaje>> historial_conversaciones = new Dictionary<int, List<Mensaje>>();
-    private Mensaje nuevo_mensaje;    // para cuando escribamos un mensaje nuevo podamos almacenarlo
 
 
     void Start()
     {
-        AgenteComunicativo.RegistrarAgente(this);
         agent = GetComponent<NavMeshAgent>();
+        comunicacion = GetComponent<ComunicacionGuardian>();
         patrullar = GetComponent<Patrullar>();
         perseguir = GetComponent<Perseguir>();
         investigar = GetComponent<Investigar>();
@@ -66,6 +62,7 @@ public class Cerebro : MonoBehaviour, InterfazAgenteComunicativo
         estado.Comportamiento();
     }
 
+
     public void VeAlLadron(Vector3 posicion)
     {
         estado = perseguir;
@@ -73,27 +70,9 @@ public class Cerebro : MonoBehaviour, InterfazAgenteComunicativo
         ultimaPosicionConocida = posicion;
         tiempoUltimaVision = Time.time;
 
-        Debug.Log(name + " ha visto al ladrón");
-        propuestas.Clear();
-        ID_actual++;
-        esperandoRespuestas = true;
-
-        nuevo_mensaje = new Mensaje
-            {
-                Emisor = gameObject,
-                intencion = Intencion.Cfp,
-                Contenido = "Ladrón visto" + "|" + posicion.x + ";" + posicion.y + ";" + posicion.z,
-                IDConversacion = ID_actual
-            };
-
-        RegistrarEnHistorial(nuevo_mensaje);
-
-        AgenteComunicativo.EnviarBroadcast(nuevo_mensaje);
-
-        CancelInvoke("SeleccionarGanador");     // cancelamos selecciones pasadas para que no acepte propuestas antiguas
-        Invoke("SeleccionarGanador", 1.0f);
-
+        comunicacion.CFP_LadronVisto(posicion);
     }
+
 
     public void EscuchaAlLadron(Vector3 puntoInvestigacion)
     {
@@ -103,245 +82,39 @@ public class Cerebro : MonoBehaviour, InterfazAgenteComunicativo
             investigar.posicion = puntoInvestigacion;
             tiempoUltimaInvestigación = Time.time;
 
-            Debug.Log(name + " ha escuchado al ladrón");
-            propuestas.Clear();
-            ID_actual++;
-            esperandoRespuestas = true;
-
-            nuevo_mensaje = new Mensaje
-                {
-                    Emisor = gameObject,
-                    intencion = Intencion.Cfp,
-                    Contenido = "Ladrón escuchado" + "|" + puntoInvestigacion.x + ";" + puntoInvestigacion.y + ";" + puntoInvestigacion.z,
-                    IDConversacion = ID_actual
-                };
-
-            RegistrarEnHistorial(nuevo_mensaje);
-
-            AgenteComunicativo.EnviarBroadcast(nuevo_mensaje);
-
-            CancelInvoke("SeleccionarGanador");     // cancelamos selecciones pasadas para que no acepte propuestas antiguas
-            Invoke("SeleccionarGanador", 1.0f); 
+            comunicacion.CFP_LadronEscuchado(puntoInvestigacion);
         }
     }
 
+
     public void TrofeoAusente(bool trofeo_robado)
     {
-        Debug.Log(name + " ha visto que han robado el tesoro");
-        propuestas.Clear();
-        ID_actual++;
-        esperandoRespuestas = true;
+        comunicacion.CFP_TesoroRobado(PuntoHuidaLadron.position);
+    }
 
-        nuevo_mensaje = new Mensaje
-            {
-                Emisor = gameObject,
-                intencion = Intencion.Cfp,
-                Contenido = "Tesoro robado" + "|" + PuntoHuidaLadron.position.x + ";" + PuntoHuidaLadron.position.y + ";" + PuntoHuidaLadron.position.z,
-                IDConversacion = ID_actual
-            };
 
-        RegistrarEnHistorial(nuevo_mensaje);
+    public void IrAPerseguir(Vector3 pos)
+    {
+        estado = perseguir;
+        perseguir.posicion = pos;
+    }
 
-        AgenteComunicativo.EnviarBroadcast(nuevo_mensaje);
 
-        CancelInvoke("SeleccionarGanador");     // cancelamos selecciones pasadas para que no acepte propuestas antiguas
-        Invoke("SeleccionarGanador", 1.0f);    }
+    public void IrAInvestigar(Vector3 pos)
+    {
+        estado = investigar;
+        investigar.posicion = pos;
+    }
+
 
     public void TocaAlLadron()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
-    public void RecibirMensaje(Mensaje mensaje)
-    {
-        RegistrarEnHistorial(mensaje);
-
-        switch (mensaje.intencion)
-        {
-            case Intencion.Cfp:
-
-                if (estado is Perseguir){
-
-                    nuevo_mensaje = new Mensaje
-                        {
-                            Emisor = gameObject,
-                            Receptor = mensaje.Emisor,
-                            intencion = Intencion.Refuse,
-                            IDConversacion = mensaje.IDConversacion
-                        };
-
-                    RegistrarEnHistorial(nuevo_mensaje);
-
-                    AgenteComunicativo.EnviarDirecto(mensaje.Emisor, nuevo_mensaje);
-
-                    break;    
-                }
-
-
-                // calculamos primero la distancia del guardián al ladrón (contenido del mensaje)
-                var (accion, posicion) = LeerContenido(mensaje.Contenido);
-                ultimaPosicionConocida = posicion;
-                float distancia = Vector3.Distance(transform.position, ultimaPosicionConocida);
-
-                nuevo_mensaje = new Mensaje
-                {
-                    Emisor = gameObject,
-                    Receptor = mensaje.Emisor,
-                    intencion = Intencion.Propose,
-                    Contenido = distancia.ToString(),
-                    IDConversacion = mensaje.IDConversacion
-                };
-
-                RegistrarEnHistorial(nuevo_mensaje);
-
-                AgenteComunicativo.EnviarDirecto(mensaje.Emisor, nuevo_mensaje);
-
-                break;
-
-            case Intencion.AcceptProposal:
-            
-                if (mensaje.IDConversacion != ID_actual)
-                    break;
-
-                var (accion2, posicion2) = LeerContenido(historial_conversaciones[ID_actual][0].Contenido);   // obtenemos del historial el contenido del CFP original de esa conversación
-                ultimaPosicionConocida = posicion2;
-
-                if (accion2 is "Ladrón visto")
-                {
-                    estado = perseguir;
-                    perseguir.posicion = ultimaPosicionConocida;
-                    Debug.Log(name + " aceptado para perseguir");
-                }
-
-                else if (accion2 is "Ladrón escuchado")
-                {
-                    estado = investigar;
-                    investigar.posicion = ultimaPosicionConocida;
-                    Debug.Log(name + " aceptado para investigar");
-                }
-
-                else if (accion2 is "Tesoro robado")
-                {
-                    estado = perseguir;
-                    perseguir.posicion = ultimaPosicionConocida;
-                    Debug.Log(name + " aceptado para evitar que ladrón escape");
-                }
-
-                break;
-
-            case Intencion.RejectProposal:
-
-                Debug.Log(name + " rechazado");
-
-                break;
-
-            case Intencion.Propose:
-
-                if (mensaje.IDConversacion == ID_actual)
-                {
-                    float valor = float.Parse(mensaje.Contenido);
-                    propuestas[mensaje.Emisor] = valor;
-                    Debug.Log("Propuesta de " + mensaje.Emisor.name + ": " + valor);
-                }
-
-                break;
-        }
-    }
-
-    (string, Vector3) LeerContenido(string contenido)
-    {
-        string[] partes = contenido.Split("|");
-        
-        string accion = partes[0];
-        string posicion = partes[1];
-
-        string[] coords = posicion.Split(';');
-
-        float x = float.Parse(coords[0]);
-        float y = float.Parse(coords[1]);
-        float z = float.Parse(coords[2]);
-
-        return (accion, new Vector3(x, y, z));
-    }
-
-    private void RegistrarEnHistorial(Mensaje mensaje)
-    {
-        if (!historial_conversaciones.ContainsKey(mensaje.IDConversacion))
-        {
-            historial_conversaciones[mensaje.IDConversacion] = new List<Mensaje>();
-        }
-        historial_conversaciones[mensaje.IDConversacion].Add(mensaje);
-    }
-
-    void SeleccionarGanador()
-    {
-        if (!esperandoRespuestas) return;
-
-        esperandoRespuestas = false;
-
-        if (propuestas.Count == 0)
-        {
-            Debug.Log("Ningún guardián respondió");
-            return;
-        }
-
-        GameObject mejorGuardian = null;
-        float mejorValor = Mathf.Infinity;
-
-        foreach (var propuesta in propuestas)
-        {
-            if (propuesta.Value < mejorValor)
-            {
-                mejorValor = propuesta.Value;
-                mejorGuardian = propuesta.Key;
-            }
-        }
-
-        foreach (var propuesta in propuestas)
-        {
-            if (propuesta.Key == mejorGuardian)
-            {
-
-                nuevo_mensaje = new Mensaje
-                    {
-                        Emisor = gameObject,
-                        Receptor = propuesta.Key,
-                        intencion = Intencion.AcceptProposal,
-                        IDConversacion = ID_actual
-                    };
-
-                RegistrarEnHistorial(nuevo_mensaje);
-
-                AgenteComunicativo.EnviarDirecto(propuesta.Key, nuevo_mensaje);
-
-                
-            }
-            else
-            {
-                nuevo_mensaje = new Mensaje
-                    {
-                        Emisor = gameObject,
-                        Receptor = propuesta.Key,
-                        intencion = Intencion.RejectProposal,
-                        IDConversacion = ID_actual
-                    };
-
-                RegistrarEnHistorial(nuevo_mensaje);
-
-                AgenteComunicativo.EnviarDirecto(propuesta.Key, nuevo_mensaje);
-            }
-        }
-
-        Debug.Log("Ganador: " + mejorGuardian.name);
-    }
 
     public GameObject GetGameObject()
     {
         return gameObject;
-    }
-
-    void OnDestroy()
-    {
-        AgenteComunicativo.EliminarAgente(this);
     }
 }
